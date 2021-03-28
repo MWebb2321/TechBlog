9d77559aeb8bad8da8e62e3f395a81222fb35332
Matthew Webb <mwebb2321@gmail.com>
2021-03-28 12:16:49 -0700

    started server.js file

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const session = require("express-session");
+const hbs = require("express-handlebars");
+const sequelize = require("./config/connection");
+const SequelizeStore = "connect-session-sequelize"(session.Store);
+const routes = require("./controllers");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const handlebarsHelpers = require("./utils/helpers");
+const exphbs = hbs({ helpers: handlebarsHelpers });
+
+app.use(express.json());
+app.use(express.urlencoded({ extends: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+const sess = {
+  secret: "super secret secret",
+  cookie: {
+    //Session expires in 10 minutes
+    maxAge: 10 * 60 * 1000,
+  },
+  resave: false,
+};
