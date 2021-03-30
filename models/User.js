f28eda9ee53b499f6997b0d4d1b2d08d46d2040c
Matthew Webb <mwebb2321@gmail.com>
2021-03-29 22:35:57 -0700

    more login/signup/update schtuff

--- models/User.js
+++ models/User.js
@@ -11,42 +11,65 @@ class User extends Model {
 }
 
 //Define table columns and configuration
-User.init({
-  //Define an id column
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  //Define a username column
-  username: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  twitter: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  github: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  //Define an email column
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true,
+User.init(
+  {
+    //Define an id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    //Define a username column
+    username: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    twitter: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    github: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    //Define an email column
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    //Define a password column
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4],
+      },
     },
   },
-  //Define a password column
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      len: [4],
+  {
+    hooks: {
+      //Set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      //Set up beforeUpdate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt(updatedUserData.password, 10);
+        return updatedUserData;
+      },
     },
-  },
-});
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
