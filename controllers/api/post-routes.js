09dde563687289d32ae61d912773ae7f7179f169
Matthew Webb <mwebb2321@gmail.com>
2021-03-29 18:52:37 -0700

    more work on post-routes

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,68 @@
+const router = require("express").Router();
+const { Post, User, Comment } = require("../../models");
+const sequelize = require("../../config/connection");
+const withAuth = require("../../utils/auth");
+
+//Gets all users
+router.get("/", (req, res) => {
+  console.log("==========");
+  Post.findAll({
+    attributes: ["id", "title", "created_at", "post_content"],
+    order: [['created_at, "DESC']],
+    include: [
+      //Comment model here -- attached username to comment
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username", "twitter", "github"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username", "twitter", "github"],
+      },
+    ],
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "created_id", "post-content"],
+    include: [
+      //Include comment model here:
+      {
+        model: User,
+        attributes: ["username", "twitter", "github"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username", "twitter", "github"],
+        },
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ mesage: "No post found with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
