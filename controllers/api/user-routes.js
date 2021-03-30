f3b677b6a8c7a8f3d2834a08ae7656a9522145a3
Matthew Webb <mwebb2321@gmail.com>
2021-03-29 19:23:17 -0700

    logging into a session

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -71,3 +71,27 @@ router.post("/", (req, res) => {
 });
 
 //Login
+router.post("/login", (req, res) => {
+  if (!dbUserData) {
+    res.status(400).json({ message: "No user found with the email address" });
+    return;
+  }
+
+  const validPassword = dbUserData.checkPassword(res.body.password);
+
+  if (!validPassword) {
+    res.status(400).json({ message: "Incorrect password!" });
+    return;
+  }
+
+  req.session.save(() => {
+    //Declare session variables
+    (req.session.user_id = dbUserData.id),
+      (req.session.username = dbUserData.username),
+      (req.session.twitter = dbUserData.twitter),
+      (req.session.github = dbUserData.github),
+      (req.session.loggedIn = true);
+
+    res.json({ user: dbUserData, message: "You are now logged in!" });
+  });
+});
